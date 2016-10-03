4e3bb7eb50d217a325ed568300ac8003207a793d
AMangano <andreaman87@gmail.com>
2016-10-03 22:29:30 +0200

    Bound command server with starter npm command

--- src/cmds/server.js
+++ src/cmds/server.js
@@ -1,15 +1,21 @@
 'use strict';
 
+const shell = require("shelljs");
 const cwd = require('cwd');
 const path = require('path');
 //const pkg = require(path.join(cwd(), 'package.json'));
 
+const chalk = require( 'chalk' );
+
+var noticeStyle = chalk.bold.yellow;
+
 // Serve Command
 //--------------
-exports.command = 'serve';
+exports.command = 'server';
 
 exports.desc = 'Serve the ./build folder';
 
 exports.handler = function ( argv ) {
-  console.log('Serving the build folder.')
+  console.log(noticeStyle('Serve the build folder...'));
+  shell.exec('npm run serve');
 };
