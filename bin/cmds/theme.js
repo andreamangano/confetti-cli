8a5cf56744d43e17de6a63899dd55907f2d10390
AMangano <andreaman87@gmail.com>
2016-12-03 19:23:37 +0100

    Removed bin folder.

--- bin/cmds/theme.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.command = 'theme <command>';
-exports.desc = 'Manage set of themes';
-exports.builder = function (yargs) {
-  return yargs.commandDir('theme_cmds');
-};
-exports.handler = function (argv) {};
