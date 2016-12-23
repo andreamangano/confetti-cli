d6c183754d51114a876969e4948d5e19a05a29cc
AMangano <andreaman87@gmail.com>
2016-12-23 21:19:06 +0100

    Adds settings for loader.

---
+++ src/config-loader.js
@@ -0,0 +1,14 @@
+module.exports = {
+  paths: {
+    settings: './settings.yml',
+    slides: './data/slides.yml',
+    images: './data/images/',
+    themes: './themes',
+    dist: './dist'
+  },
+  compilers: {
+    sass: {
+      includePaths: ['./node_modules/']
+    }
+  }
+};
