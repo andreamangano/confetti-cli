f42e69724a14e6b7b1a077bf40994ff4c566de20
AMangano <andreaman87@gmail.com>
2016-12-23 21:18:38 +0100

    Refactors generate method by using lib rather than bin files.

--- src/cmds/generate.js
+++ src/cmds/generate.js
@@ -1,16 +1,37 @@
 'use strict';
 import shell from 'shelljs';
 import chalk from 'chalk';
+import configLoader from './../config-loader';
 import config from './../config';
+import Generator from 'confetti-generator';
+import Loader from 'confetti-loader';
 const errorStyle = chalk.bold.red;
+const successStyle = chalk.bold.green;
 // Generate Command
 //--------------
 exports.command = 'generate';
 exports.desc = 'Generate all assets for the speaker deck.';
 exports.handler = argv => {
+  const loader = new Loader();
+  loader.loadDeck(configLoader)
+    .then(deckData => {
+      const generator = new Generator(deckData.paths, true);
+      generator.generate(deckData)
+        .then(() => {
+          console.log(successStyle('Speaker Deck built.'));
+        })
+        .catch(error => {
+          console.log(errorStyle(error));
+        });
+    })
+    .catch(error => {
+      console.log(errorStyle(error));
+    });
+
   // TODO: clear dist folder before
-  if (shell.exec('confetti-loader --config confetti.loader.json | confetti-generator').code !== 0) {
-    console.error(errorStyle('Error: Generation failed.'));
-    shell.exit(1);
-  }
+  // if (shell.exec('confetti-loader --config confetti.loader.json |
+  // confetti-generator').code !== 0) {
+  //  console.error(errorStyle('Error: Generation failed.'));
+  //  shell.exit(1);
+  // }
 };
