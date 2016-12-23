fe22f7d361c9a92f98d77ae5f31b5c224149b4e2
AMangano <andreaman87@gmail.com>
2016-12-23 21:18:54 +0100

    Integrates server command.

--- src/cmds/server.js
+++ src/cmds/server.js
@@ -1,15 +1,18 @@
 'use strict';
 import shell from 'shelljs';
+import bs from 'browser-sync';
 import config from '../config.js';
 const chalk = require('chalk');
 const noticeStyle = chalk.bold.yellow;
 // Serve Command
 //--------------
 exports.command = 'server';
-exports.desc = 'Serve the ./build folder';
+exports.desc = `Serve the ${config.DIST_FOLDER} folder`;
 exports.handler = argv => {
-  shell.cd(config.STARTER_FOLDER);
-  shell.cd(config.STARTER_REPOSITORY_NAME);
-  console.log(noticeStyle('Serve the build folder...'));
-  shell.exec('npm run serve');
+  console.log(noticeStyle('Serve the dist folder...'));
+  bs.create('Confetti Server');
+  bs.init({
+    server: config.DIST_FOLDER
+  });
+  bs.watch(config.DIST_FOLDER).on('change', bs.reload);
 };
