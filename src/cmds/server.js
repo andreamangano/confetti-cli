2689e325fc4fa3cee57a968610209a142f5a1e63
AMangano <andreaman87@gmail.com>
2016-12-24 14:15:02 +0100

    Integrates browser sync for serving dish folder.

--- src/cmds/server.js
+++ src/cmds/server.js
@@ -2,17 +2,24 @@
 import shell from 'shelljs';
 import bs from 'browser-sync';
 import config from '../config.js';
-const chalk = require('chalk');
-const noticeStyle = chalk.bold.yellow;
+import * as logger from './../logger';
+//--------------
 // Serve Command
 //--------------
 exports.command = 'server';
 exports.desc = `Serve the ${config.DIST_FOLDER} folder`;
 exports.handler = argv => {
-  console.log(noticeStyle('Serve the dist folder...'));
-  bs.create('Confetti Server');
-  bs.init({
-    server: config.DIST_FOLDER
-  });
-  bs.watch(config.DIST_FOLDER).on('change', bs.reload);
+  if (shell.exec('confetti generate').code === 0) {
+    logger.success('All done! Enjoy Confetti!', 0x1F389);
+    logger.message('Serving the dist folder...');
+    bs.create('Confetti Server');
+    bs.init({
+      server: config.DIST_FOLDER
+    });
+    bs.watch(config.DIST_FOLDER).on('change', bs.reload);
+  } else {
+    logger.error(`Error occurs during the building process.`);
+    shell.exit(1);
+  }
 };
+
