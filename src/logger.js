5dbf4dd166a4b73358efc22d65bd7ac2db020e3e
AMangano <andreaman87@gmail.com>
2017-01-07 18:54:39 +0100

    Improves log message

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+const prefixLog = '[Confetti]';
 
 export function message(msg) {
   console.log(`${chalk.bold.gray(msg)}`);
@@ -6,20 +7,20 @@ export function message(msg) {
 
 export function info(msg, iconCode) {
   const icon = String.fromCodePoint(iconCode ? iconCode : 0x1F4E2);
-  console.log(`${icon}  ${chalk.bold.underline.blue('Info:')} --> ${chalk.bold.blue(msg)}`);
+  console.log(`${icon} ${chalk.bold.blue(prefixLog)} ==> ${chalk.bold.blue(msg)}`);
 }
 
 export function error(msg, iconCode) {
   const icon = String.fromCodePoint(iconCode ? iconCode : 0x2757);
-  console.log(`${icon} ${chalk.bold.red(msg)}`);
+  console.log(`${icon} ${chalk.bold.red(prefixLog)} ==> ${chalk.bold.red(msg)}`);
 }
 
 export function warning(msg, iconCode) {
   const icon = String.fromCodePoint(iconCode ? iconCode : 0x26A0);
-  console.log(`${icon}  ${chalk.bold.underline.yellow('Warning:')} --> ${chalk.bold.yellow(msg)}`);
+  console.log(`${icon} ${chalk.bold.yellow(prefixLog)} ==> ${chalk.bold.yellow(msg)}`);
 }
 
 export function success(msg, iconCode) {
   const icon = String.fromCodePoint(iconCode ? iconCode : 0x1F44C);
-  console.log(`${icon}  ${chalk.bold.underline.green('Success:')} --> ${chalk.bold.green(msg)}`);
+  console.log(`${icon} ${chalk.bold.green(prefixLog)} ==> ${chalk.bold.green(msg)}`);
 }
