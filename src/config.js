e694d450aa1746cadae647716b91a422f1dbabe1
AMangano <andreaman87@gmail.com>
2017-01-08 13:50:05 +0100

    Replace default theme

--- src/config.js
+++ src/config.js
@@ -2,6 +2,10 @@
 const THEME_PREFIX = 'confetti-theme-';
 // List of official themes
 const official_themes = {
+  voice: {
+    name: 'voice',
+    repository: `https://github.com/andreamangano/${THEME_PREFIX}voice.git`
+  },
   lena: {
     name: 'lena',
     repository: `https://github.com/andreamangano/${THEME_PREFIX}lena.git`
@@ -19,6 +23,6 @@ export default {
   DIST_FOLDER: './dist',
   DEV_FOLDER: './dev',
   THEME_PREFIX,
-  DEFAULT_THEME: official_themes.tangerine,
+  DEFAULT_THEME: official_themes.voice,
   OFFICIAL_THEMES: official_themes
 };
