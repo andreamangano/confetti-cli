ad22c79322c51d5429e9c87d7329765798c8c122
AMangano <andreaman87@gmail.com>
2017-01-14 14:37:50 +0100

    Avoids to remove the dist folder (it could be versioned)

--- src/cmds/build.js
+++ src/cmds/build.js
@@ -5,7 +5,6 @@ import configLoader from './../config-loader';
 import Generator from 'confetti-generator';
 import Loader from 'confetti-loader';
 import * as logger from './../logger';
-import del from 'del';
 const loader = new Loader();
 //--------------
 // Serve Command
@@ -13,22 +12,16 @@ const loader = new Loader();
 exports.command = 'build';
 exports.desc = `Build the dist folder (${config.DIST_FOLDER})`;
 exports.handler = argv => {
-  del(config.DIST_FOLDER)
-    .then(() => {
-      logger.message('Loading speaker deck data...');
-      loader.loadDeck(configLoader, true)
-        .then(deckData => {
-          const generator = new Generator(deckData.paths, true);
-          logger.success('Speaker deck data loaded.');
-          logger.message('Generating speaker deck...');
-          generator.generate(deckData)
-            .then(() => {
-              logger.success('Build done!');
-            })
-            .catch(error => {
-              logger.error(error);
-              shell.exit(1);
-            });
+  // Don't remove the dist folder before because it could be versioned.
+  logger.message('Loading speaker deck data...');
+  loader.loadDeck(configLoader, true)
+    .then(deckData => {
+      const generator = new Generator(deckData.paths, true);
+      logger.success('Speaker deck data loaded.');
+      logger.message('Generating speaker deck for build...');
+      generator.generate(deckData)
+        .then(() => {
+          logger.success('Build done! The dist folder has been generated.');
         })
         .catch(error => {
           logger.error(error);
